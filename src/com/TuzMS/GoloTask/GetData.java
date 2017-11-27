f94057e75d5683c769da5b662b8bc6bdbf671a69
Михаил Тужилкин < >
2017-11-27 22:01:33 +0300

    Удачное чтение из файла

---
+++ src/com/TuzMS/GoloTask/GetData.java
@@ -0,0 +1,39 @@
+package com.TuzMS.GoloTask;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.File;
+import java.io.BufferedReader;
+
+public class GetData {
+	private final String taskFileName = "Task.txt"; //Название файла с заданием
+	private float[] arrX; //Массив значений X
+	private float[] arrY; //Массив значений Y
+	
+	public void readFile(String fileName) {
+		try {
+			File taskFile = new File(taskFileName); //Файл открыт
+			FileReader readTaskFile = new FileReader(taskFile); //Файл открыт для чтения
+			BufferedReader reader = new BufferedReader(readTaskFile); //Читатель файла
+			
+			String line = reader.readLine();
+			
+			for (int i = 0; i <=11; i++){
+				line = reader.readLine();
+				//Отмотка до строки со значениями
+			}
+			
+			while (line != null){
+				System.out.println(line);
+				line = reader.readLine();
+			}
+			System.out.println("Готово");
+		} catch (IOException ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+	
+	public String getTaskFileName() {
+		return taskFileName;
+	}
+}
--- src/com/TuzMS/GoloTask/Main.java
+++ src/com/TuzMS/GoloTask/Main.java
@@ -1,5 +1,9 @@
 package com.TuzMS.GoloTask;
 
 public class Main {
-
+	public static void main(String [] args) {
+		GetData d = new GetData();
+    	d.readFile(d.getTaskFileName());
+    	System.out.println("Good");
+	}
 }
