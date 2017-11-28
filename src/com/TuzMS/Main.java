ae496a9e49638b7dda91f689d7dd35db13ad26b7
Михаил Тужилкин < >
2017-11-28 23:52:16 +0300

    Заебало всё, переделываю через эклипс (reverted from commit 60c5cbf568fe55f2d8f9fd541a69bf961e98c2f2)

--- src/com/TuzMS/Main.java
+++ src/com/TuzMS/Main.java
@@ -3,8 +3,6 @@ package com.TuzMS;
 public class Main {
 
     public static void main(String[] args) {
-    	GetData d = new GetData();
-    	d.readFile(d.getTaskFileName());
-    	System.out.println("jhfdsajhfdsjh");
+	// write your code here
     }
 }
