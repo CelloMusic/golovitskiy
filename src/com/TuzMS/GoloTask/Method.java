def4bc233cfe840dc1e04f96580b9b440c4b1c64
Михаил Тужилкин < >
2017-11-28 22:46:50 +0300

    Запилил МНК

--- src/com/TuzMS/GoloTask/Method.java
+++ src/com/TuzMS/GoloTask/Method.java
@@ -2,24 +2,24 @@ package com.TuzMS.GoloTask;
 
 public class Method {
 	//Родительский класс для описания методов
-		private double[] Xi; //Значения Х точек
-		private double[] Yi; //Значения Y точек
-		private double[] a = new double[3]; //Коэффициенты аппроксимирующей зависимости
-		private int n; //Количесво точек
+	protected double[] Xi; //Значения Х точек
+	protected double[] Yi; //Значения Y точек
+	protected double[] a = new double[3]; //Коэффициенты аппроксимирующей зависимости
+	protected int k; //Количесво точек
 		
-		public Method(double[] x, double[] y){
-			Xi = x;
-			Yi = y;
-			n = Xi.length;
-		}
+	public Method(double[] x, double[] y){
+		Xi = x;
+		Yi = y;
+		k = Xi.length;
+	}
 		
-		protected double[] ResApproks() {
-			double[] YRes = new double[n];
-			for (int j = 0; j < n; j++) {
-				YRes[j] = a[1]*Xi[j] + a[2]*Math.pow(Xi[j], 3)
-						+ a[3]*Math.pow(Xi[j], 5);
-			}
-			return YRes;
+	protected double[] ResApproks() {
+		double[] YRes = new double[k];
+		for (int j = 0; j < k; j++) {
+			YRes[j] = a[1]*Xi[j] + a[2]*Math.pow(Xi[j], 3)
+					+ a[3]*Math.pow(Xi[j], 5);
 		}
+		return YRes;
+	}
 
 }
